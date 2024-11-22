f5cb57704b3a74d651adbc9290c2ffc147ab93bd
Spillehus <c.spillehus@live.dk>
2024-11-22 11:01:43 +0100

    UPload:)

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,6 +5,11 @@ require('dotenv').config();
 
 // Authenticate user
 const authenticateUser = async (username, password) => {
+
+  console.log("Url: ", process.env.LDAP_URL);
+  console.log("User: ", process.env.LDAP_USER);
+  console.log("Pass: ", process.env.LDAP_PASS);
+
   // Connect to the LDAP server
   const ad = new AD({
     url: process.env.LDAP_URL,
